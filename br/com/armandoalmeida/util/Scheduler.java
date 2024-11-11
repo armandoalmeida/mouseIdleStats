df8879b3334ab9247e4fbd77b19d72ea3cae35b3
Jose Neto <jose.neto@kuehne-nagel.com>
2024-11-11 00:54:08 +0000

    Refactoring

--- br/com/armandoalmeida/util/Scheduler.java
+++ br/com/armandoalmeida/util/Scheduler.java
@@ -6,6 +6,27 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * The Scheduler class provides a mechanism to schedule tasks for execution
+ * either periodically or as a one-time event. It implements the Runnable
+ * interface, allowing it to be executed in a separate thread.
+ *
+ * <p>This class supports two types of scheduling:</p>
+ * <ul>
+ *     <li><strong>One-Shot:</strong> Executes the task once after a specified delay.</li>
+ *     <li><strong>Periodic:</strong> Executes the task repeatedly at fixed intervals.</li>
+ * </ul>
+ *
+ * <p>Example usage:</p>
+ * <pre>
+ * Scheduler periodicScheduler = Scheduler.newPeriodic(() -> System.out.println("Periodic Task"), Duration.ofSeconds(5));
+ * periodicScheduler.run();
+ * // OR
+ * new Thread(periodicScheduler).start();
+ *
+ * Scheduler.oneShot(() -> System.out.println("One-Shot Task"), Duration.ofSeconds(10));
+ * </pre>
+ */
 public class Scheduler implements Runnable {
 
     private final Runnable command;
@@ -21,10 +42,24 @@ public class Scheduler implements Runnable {
         this.scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
     }
 
+    /**
+     * Creates a new periodic scheduler that executes the specified command
+     * at fixed intervals.
+     *
+     * @param command  the task to be executed
+     * @param duration the duration between executions
+     * @return a new Scheduler instance
+     */
     public static Scheduler newPeriodic(Runnable command, Duration duration) {
         return new Scheduler(SchedulerType.PERIODIC, command, duration);
     }
 
+    /**
+     * Schedules a one-time execution of the specified command after the given duration.
+     *
+     * @param command  the task to be executed
+     * @param duration the delay before execution
+     */
     public static void oneShot(Runnable command, Duration duration) {
         new Scheduler(SchedulerType.ONE_SHOT, command, duration).run();
     }
@@ -36,15 +71,18 @@ public class Scheduler implements Runnable {
                 command.run();
                 this.shutdown();
             }, duration);
-        }
-
-        if (SchedulerType.PERIODIC.equals(schedulerType))
+        } else if (SchedulerType.PERIODIC.equals(schedulerType)) {
             this.scheduleAtFixedRate(command, duration);
+        }
     }
 
+    /**
+     * Shuts down the scheduler, canceling any scheduled tasks and releasing resources.
+     */
     public void shutdown() {
-        if (this.scheduledFuture != null)
+        if (this.scheduledFuture != null) {
             this.scheduledFuture.cancel(false);
+        }
         this.scheduledExecutorService.shutdown();
     }
 
@@ -59,5 +97,4 @@ public class Scheduler implements Runnable {
     private enum SchedulerType {
         PERIODIC, ONE_SHOT
     }
-
 }
